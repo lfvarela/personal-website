d84a5e20c721768e156c6bfd9bbbafb63ec1cb94
lfvarela <lfvarela@stanford.edu>
2017-08-23 23:14:26 -0700

    Simple update app.js

--- app.js
+++ app.js
@@ -8,16 +8,3 @@ app.use('/', express.static(__dirname + '/public'));
 app.listen(port, function() {
   console.log('listening on port ' + port)
 });
-
-
-var bittrex = require('node.bittrex.api');
-
-var Ethereum = module.exports = {
-    price: 0,
-
-    updatePrice: function() {
-      bittrex.getticker( {market:'BTC-LTC'}, function(data, err) {
-        Ethereumeth.price = data;
-      });
-    },
-}
